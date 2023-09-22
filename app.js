4f7aa6c7f9b09beaf071e4d807e11dd9af0f4628
Aditya Badhe <89151645+abadhe@users.noreply.github.com>
2023-09-22 11:23:20 -0600

    Add new feature and fix bug

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-
-/*
-
-        <div id = "parent">
-            <div id = "child1">
-                <h1>"This is a h1 tag"</h1>
-                <h2>"This is a h2 tag"</h2>
-            </div>
-
-            <div id = "child2">
-                <h1>"This is a h1 tag"</h1>
-                <h2>"This is a h2 tag"</h2>
-            </div>
-
-        <div>
-
-
-
-
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is a h1 tag lavde"),
-    React.createElement("h2", {}, "This is a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "This is a h1 tag"),
-    React.createElement("h2", {}, "This is a h2 tag"),
-  ]),
-]);
+const heading  = React.createElement("h1", {id: "heading"}, "This is react heading");
+
+const jsxheading = <h1 id = "heading">This is jsx heading</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
