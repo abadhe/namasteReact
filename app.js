18778191969dbe88fa4e081c71ae61a22bd2e218
Aditya Badhe <89151645+abadhe@users.noreply.github.com>
2023-09-22 10:31:32 -0600

    commit changeds

--- app.js
+++ app.js
@@ -19,17 +19,19 @@
 
 */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is a h1 tag lavde"),
+    React.createElement("h2", {}, "This is a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is a h1 tag"),
+    React.createElement("h2", {}, "This is a h2 tag"),
+  ]),
+]);
 
-
-const parent  = React.createElement("div", {id : "parent"}, 
-    
-[React.createElement("div", {id : "child1"}, [React.createElement("h1", {}, "This is a h1 tag lavde"), React.createElement("h2", {}, "This is a h2 tag")]),
-
-React.createElement("div", {id : "child2"}, [React.createElement("h1", {}, "This is a h1 tag"), React.createElement("h2", {}, "This is a h2 tag")])
-]
-
-)
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
