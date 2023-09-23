bf19f99aa765468f4e24f27d7ef60c769b805273
Aditya Badhe <89151645+abadhe@users.noreply.github.com>
2023-09-22 19:04:19 -0600

    last of version 2.0

--- app.js
+++ app.js
@@ -5,5 +5,26 @@ const heading  = React.createElement("h1", {id: "heading"}, "This is react headi
 
 const jsxheading = <h1 id = "heading">This is jsx heading</h1>;
 
+// const TitleComponent = () => (
+//     <h1>
+//         My title Component
+//     </h1>
+// )
+const title = (
+    <h1>
+        Title React Object
+    </h1>
+)
+
+const HeadingComponent = () => (
+    <div>
+        {/* <TitleComponent></TitleComponent> */}
+        {title}
+        <h2>
+            My heading component
+        </h2>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
